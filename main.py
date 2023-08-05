eaa70b716d09e720950ce4ff75e7ac8b9ebebc6c
Kushal Banda <kushalbanda265@gmail.com>
2023-08-05 10:59:22 +0530

    Model Evaluation Added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.CNNClassifier import logger
 from src.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.CNNClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from src.CNNClassifier.pipeline.stage_04_evaluation import EvaluationPipeline 
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -45,3 +46,16 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise(e)
+    
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
